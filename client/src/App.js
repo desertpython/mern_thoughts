ee3bdaeae6de4220a6da352dbef4be96b252ead1
Desert Python <mattsenneff@aol.com>
2020-10-14 18:37:02 -0700

    imports

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,16 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql'
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className='flex-column justify-flex-start min-100-vh'>
       <Header />
       <div className='container'>
@@ -14,6 +22,7 @@ function App() {
       </div>
       <Footer />
     </div>
+    </ApolloProvider>
   );
 }
 
